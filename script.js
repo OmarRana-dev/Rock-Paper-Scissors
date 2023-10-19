3bc4bef96217bacbbec4d2db163ab5c48b255046
Omar Rana <sharifzada586@gmail.com>
2023-10-19 19:25:59 +0500

    I made some of the changing in JS file like i add five win rule, show crunt rounds, show crunt results, show after match finel result.

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const resultContainer = document.querySelector("#result-container");
 const btnRock = document.querySelector("#btn-rock");
 const btnScissor = document.querySelector("#btn-scissor");
 const btnPaper = document.querySelector("#btn-paper");
-
+const finelResult = document.querySelector("#finel-result");
+const humanWinsRound = document.querySelector("#win-rounds");
+const computerWindsRound = document.querySelector("#lose-rounds");
+const roundCounter = document.querySelector("#roundCounter");
 
 btnRock.addEventListener("click", function (e) {
   userContainer.textContent = e.target.textContent;
@@ -30,9 +33,15 @@ function getComputerChoice() {
 // this function called in game() function to check the player-input and computer choice who will win ro loss.
 
 let n = 0;
-
+let win = 0;
+let lose = 0;
+let wImjs = "&#129395;&#129395;&#129395;";
+let lImjs = "&#128557;&#128557;&#128557;";
+let tImjs = "&#129309;&#129309;&#129309;";
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
+    n++;
+    roundCounter.textContent = n;
     return `Drow!`;
   } else if (
     (playerSelection === "paper" && computerSelection === "rock") ||
@@ -40,12 +49,20 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissor" && computerSelection === "paper")
   ) {
     n++;
-    return `You Win!`;
+    win++;
+    humanWinsRound.textContent = win;
+    roundCounter.textContent = n;
+    return `Win!`;
   } else {
     n++;
-    return `You Lose!`;
+    lose++;
+    computerWindsRound.textContent = lose;
+    roundCounter.textContent = n;
+    return `Lose!`;
   }
 }
+
+const matchDetails = document.querySelector("#detailed-result");
 
 function game() {
   let playerSelection = userContainer.textContent.toLowerCase();
@@ -55,10 +72,31 @@ function game() {
   // here we call to funtion that will check who's win's the game computer or player and give's result
   let result = playRound(playerSelection, computerSelection.toLowerCase());
   resultContainer.textContent = result;
-  if (n === 5){
-    console.log("You Play 5 ROUND'S.")
-    n = 0;
+  if (win === 5 || lose === 5) {
+    if (win > lose) {
+      finelResult.textContent =
+        "You Won the fucking Game! Your Father pround on you.";
+      matchDetails.textContent = `You Win ${win} rounds out of ${n} round & lose  ${lose}`
+      resultContainer.innerHTML = wImjs;
+      restart();
+    } else if (lose > win) {
+      finelResult.textContent = "Fuck You! You'r loser BigAssHole.";
+      resultContainer.innerHTML = lImjs;
+      restart();
+    } else {
+      finelResult.textContent = "Game is Drow! Try Again and win AssHole.";
+      resultContainer.innerHTML = tImjs;
+      restart();
+    }
   }
+}
+
+function restart() {
+  n = 0;
+  win = 0;
+  lose = 0;
+  computerWindsRound.textContent = lose;
+  humanWinsRound.textContent = win;
 }
 
 // Author: Omar Rana
